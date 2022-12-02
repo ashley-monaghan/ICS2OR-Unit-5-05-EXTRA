94bb8ae7b57f3f052559f935cbba1b16862fcb1c
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-02 17:49:17 +0000

    sync

--- main.cs
+++ main.cs
@@ -31,15 +31,15 @@ class Program
         lengthC = Convert.ToInt16(Console.ReadLine());
 
         Console.WriteLine(" ");
-         angleA = Math.Acos((Math.Pow(lengthB, 2) + Math.Pow(lengthC, 2) - Math.Pow(lengthA, 2)) / (2 * lengthB * lengthC)) * (180/Math.PI);
-         angleB = Math.Acos((Math.Pow(lengthC, 2) + Math.Pow(lengthA, 2) - Math.Pow(lengthB, 2)) / (2 * lengthC * lengthA)) * (180/Math.PI);
-         angleC = Math.Acos((Math.Pow(lengthA, 2) + Math.Pow(lengthB, 2) - Math.Pow(lengthC, 2)) / (2 * lengthA * lengthB)) * (180/Math.PI);
+        angleA = Math.Acos((Math.Pow(lengthB, 2) + Math.Pow(lengthC, 2) - Math.Pow(lengthA, 2)) / (2 * lengthB * lengthC)) * (180 / Math.PI);
+         angleB = Math.Acos((Math.Pow(lengthC, 2) + Math.Pow(lengthA, 2) - Math.Pow(lengthB, 2)) / (2 * lengthC * lengthA)) * (180 / Math.PI);
+         angleC = Math.Acos((Math.Pow(lengthA, 2) + Math.Pow(lengthB, 2) - Math.Pow(lengthC, 2)) / (2 * lengthA * lengthB)) * (180 / Math.PI);
 
         sumOfAngles = angleA + angleB + angleC;
         Console.WriteLine("Sum of all angles: " + sumOfAngles.ToString("0.0"));
         Console.WriteLine(" ");
 
-                      
+
         if (angleA == angleB && angleA == angleC) {
         Console.WriteLine("You have an equalatiral triangle!");
         } else if (angleA != angleB && angleA != angleC && angleB != angleC) {
